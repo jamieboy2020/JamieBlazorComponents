7c005188092bfeeb603b08ac9da6f68e77a737e6
Jamie Fraser <jamie.fraser@sqa.org.uk>
2020-02-18 21:03:52 +0000

    Add project files.

---
+++ JamieBlazorComponentLibrary/Components/AutoComplete/ListItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JamieBlazorComponentLibrary.Components.AutoComplete
+{
+    public class ListItem<T>
+    {
+        public T Item { get; set; }
+        public bool Highlighted { get; set; }
+        public bool Selected { get; set; }
+    }
+}
